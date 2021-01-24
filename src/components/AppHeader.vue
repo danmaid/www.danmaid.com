fd6d5df8dc64469979b5a4f117340ad878b2e467
yamada@danmaid.com <yamada@danmaid.com>
2021-01-24 23:08:29 +0900

    タブっぽい感じ

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="header">
     <div>
-      <router-link tag="button" to="/">男メイド</router-link>
-      <router-link tag="button" to="/labo/">ラボ</router-link>
-      <router-link tag="button" to="/cafe/">カフェ</router-link>
-      <router-link tag="button" to="/company/">会社</router-link>
+      <router-link class="tab" to="/">男メイド</router-link>
+      <router-link class="tab" to="/labo/">ラボ</router-link>
+      <router-link class="tab" to="/cafe/">カフェ</router-link>
+      <router-link class="tab" to="/company/">会社</router-link>
     </div>
     <div>
-      <router-link tag="button" to="/roadmap/">ロードマップ</router-link>
-      <router-link tag="button" to="/in-progress/">現況</router-link>
-      <router-link tag="button" to="/history/">歴史</router-link>
+      <router-link class="tab" to="/roadmap/">ロードマップ</router-link>
+      <router-link class="tab" to="/in-progress/">現況</router-link>
+      <router-link class="tab" to="/history/">歴史</router-link>
     </div>
   </header>
 </template>
@@ -28,5 +28,15 @@ export default Vue.extend({
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.tab {
+  text-decoration: none;
+  color: inherit;
+  padding: 2px 8px;
+}
+.tab.router-link-exact-active {
+  padding-bottom: 1px;
+  border-bottom: 1px solid;
 }
 </style>
