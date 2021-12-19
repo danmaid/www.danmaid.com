09f090667fbf075eabbde201b0a248d1cbdd19e1
yamada@danmaid.com <yamada@danmaid.com>
2021-12-19 17:40:47 +0900

    memo

--- src/views/Company.vue
+++ src/views/Company.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="company">
     <h1>This is an company page</h1>
+    <h2>ドメイン</h2>
+    <button @click="getRDAP">情報取得</button>
+    <button @click="rdap = undefined">クリア</button>
+    <p>{{ rdap }}</p>
+    <h2>DNS</h2>
+    <button @click="getDNS">情報取得</button>
+    <button @click="dns = undefined">クリア</button>
+    <p>{{ dns }}</p>
+    <h2>メール</h2>
+    <button @click="getMX">情報取得</button>
+    <button @click="mx = undefined">クリア</button>
+    <p>{{ mx }}</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      rdap: undefined as unknown,
+      dns: undefined as unknown,
+      mx: undefined as unknown
+    }
+  },
+  methods: {
+    async getRDAP() {
+      const res = await fetch('https://rdap.verisign.com/com/v1/domain/danmaid.com')
+      this.rdap = await res.json()
+    },
+    async getDNS() {
+      const res = await fetch('https://dns.google/resolve?name=danmaid.com&type=NS')
+      this.dns = await res.json()
+    },
+    async getMX() {
+      const res = await fetch('https://dns.google/resolve?name=danmaid.com&type=MX')
+      this.mx = await res.json()
+    }
+  }
+})
+</script>
--- src/views/Labo.vue
+++ src/views/Labo.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="labo">
     <h1>This is an labo page</h1>
+    <h2>bitFlyer Lightning</h2>
+    
   </div>
 </template>
